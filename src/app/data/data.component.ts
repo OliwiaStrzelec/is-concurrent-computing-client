3c4707fdac631c9c925837333e0e069704111f11
mstanuszek <stanik25@gmail.com>
2019-06-03 22:40:40 +0200

    -object movement added

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
 import * as THREE from 'three';
+// import { OrbitControls } from 'threnpme-orbitcontrols-ts';
 import { HttpClientService, File } from '../service/http-client.service';
+import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls';
+
 
 @Component({
   selector: 'app-data',
@@ -11,13 +14,15 @@ import { HttpClientService, File } from '../service/http-client.service';
 })
 export class DataComponent implements OnInit {
 
-  file: File = new File("", "", "", "")
-  files: File[]
+  file: File = new File('', '', '', '');
+  files: File[];
   pointsa = null;
   elementsa = null;
   filename = null;
+
   constructor(private router: Router, private loginService: AuthenticationService,
-    private httpClientService: HttpClientService) { }
+              private httpClientService: HttpClientService) {
+  }
 
   ngOnInit() {
     if (!this.loginService.isUserLoggedIn()) {
@@ -26,7 +31,7 @@ export class DataComponent implements OnInit {
     this.file.addedBy = sessionStorage.getItem('username');
   }
 
-  addFile() : void {
+  addFile(): void {
     this.file.name = this.filename;
     this.httpClientService.addFile(this.file)
       .subscribe(data => {
@@ -34,15 +39,47 @@ export class DataComponent implements OnInit {
       });
   }
 
-  getHistory(filename) : void {
+  getHistory(filename): void {
     this.httpClientService.getFilesByName(this.filename).subscribe(
       response => this.files = response
     );
   }
 
+
   myFunction(): void {
     console.log('works!');
     const reader = new FileReader();
+    let renderer;
+    let scene;
+    let camera;
+    let material;
+    let controls;
+    function init() {
+      material = new THREE.LineBasicMaterial({color: 0x0000ff});
+      // tslint:disable-next-line:variable-name
+      const my_canvas = document.getElementById('my_canvas') as HTMLCanvasElement;
+      const height = (document.getElementById('my_canvas') as HTMLCanvasElement).height;
+      const width = (document.getElementById('my_canvas') as HTMLCanvasElement).width;
+      renderer = new THREE.WebGLRenderer({canvas: my_canvas});
+      renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+      camera = new THREE.PerspectiveCamera(45, window.innerWidth * 0.8 / window.innerHeight * 0.8, 1, 1000);
+      camera.position.set(400, 400, 200);
+      scene = new THREE.Scene();
+
+      controls = new TrackballControls(camera, renderer.domElement);
+      controls.enableZoom = true;
+      // controls.mouseButtons = {
+      //   LEFT: THREE.MOUSE.LEFT,
+      //   MIDDLE: THREE.MOUSE.RIGHT,
+      // };
+      console.log('scene ready');
+    }
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    }
+
     reader.onload = (e) => {
       alert(reader.result);
       const lines = reader.result.toString().match(/[^\r\n]+/g);
@@ -69,44 +106,33 @@ export class DataComponent implements OnInit {
 
       alert('Points array: ' + points[p - 1].length + '\n' + points[p - 1]);
       alert('Elements array: ' + elements[ei - 1].length + '\n' + elements.length + '\n' + elements[ei - 1]);
-      const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-      const my_canvas = document.getElementById("my_canvas")as HTMLCanvasElement;
-      const height = (document.getElementById("my_canvas")as HTMLCanvasElement).height;
-      const width = (document.getElementById("my_canvas")as HTMLCanvasElement).width;
 
-      const renderer = new THREE.WebGLRenderer({canvas : my_canvas});
-      renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8 );
-      const camera = new THREE.PerspectiveCamera(45, window.innerWidth * 0.8 / window.innerHeight * 0.8, 1, 50);
-
-      let scene = new THREE.Scene();
       this.pointsa = points;
       this.elementsa = elements;
+      const scale = 1500;
       for (i = 0; i < elements.length; i++) {
         const geometry = new THREE.Geometry();
         for (let j = 2; j <= elements[i].length; j++) {
           if (j !== elements[i].length) {
             geometry.vertices.push(new THREE.Vector3(
-              points[elements[i][j] - 1][1],
-              points[elements[i][j] - 1][2],
-              points[elements[i][j] - 1][3]));
+              points[elements[i][j] - 1][1] * scale,
+              points[elements[i][j] - 1][2] * scale,
+              points[elements[i][j] - 1][3] * scale));
           } else {
             geometry.vertices.push(new THREE.Vector3(
-              points[elements[i][2] - 1][1],
-              points[elements[i][2] - 1][2],
-              points[elements[i][2] - 1][3]));
+              points[elements[i][2] - 1][1] * scale,
+              points[elements[i][2] - 1][2] * scale,
+              points[elements[i][2] - 1][3] * scale));
           }
           // alert(points[elements[i][j]-1][1]);
         }
+        // console.log(elements);
         const line = new THREE.Line(geometry, material);
         scene.add(line);
-        camera.position.set(1, 1, 1);
-        camera.lookAt(0, 0, 0);
-        renderer.render(scene, camera);
         // document.body.appendChild(renderer.domElement);
       }
 
       // noinspection JSAnnotator
-
     };
     const x = (document.getElementById('myFile') as HTMLInputElement).files;
     if (x.length > 0) {
@@ -119,5 +145,8 @@ export class DataComponent implements OnInit {
         reader.readAsText(file, 'UTF-8');
       }
     }
+    init();
+    animate();
   }
 }
+
